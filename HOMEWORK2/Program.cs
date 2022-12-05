a3a9e733ccaa6dd4daf29f569999cafe6f05a198
ALEXANDER <89104880360@mail.ru>
2022-12-05 15:56:18 +0300

    Домашка

---
+++ HOMEWORK2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+int a1 = 0;
+while (num>1000) 
+{
+ a1 = num/10;
+ num = a1;
+}
+
+ 
+if(num>100 && num<1000)
+{
+    int a2 = num%10;
+    Console.Write(a2);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
---
+++ HOMEWORK3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Введите цифру от 1 до 7");
+int num = int.Parse(Console.ReadLine());
+if(num == 1)
+{
+    Console.WriteLine("понедельник.Рабочий день");
+}
+if(num == 2)
+{
+    Console.WriteLine("Вторник.Рабочий день");
+}
+if(num == 3)
+{
+    Console.WriteLine("Среда.Рабочий день");
+}
+if(num == 4)
+{
+    Console.WriteLine("Четверг.Рабочий день");
+}
+if(num == 5)
+{
+    Console.WriteLine("Пятница.Рабочий день");
+}
+if(num == 6)
+{
+    Console.WriteLine("Суббота. Выходной день");
+}
+if(num == 7)
+{
+    Console.WriteLine("Воскресенье. Выходной день");
+}
